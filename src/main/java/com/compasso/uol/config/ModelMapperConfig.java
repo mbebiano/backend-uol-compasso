7c09aa7d5b89456bb79858715e2d2dd71c1bbb01
Mattheus Bebiano <mattheus.macedo@ntendencia.com.br>
2021-05-23 20:13:19 -0300

    feat: Adição de ModelMapperConfig

---
+++ src/main/java/com/compasso/uol/config/ModelMapperConfig.java
@@ -0,0 +1,13 @@
+package com.compasso.uol.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+}
